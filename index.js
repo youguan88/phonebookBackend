2e73724f433eec3b886af3eaaa93fd42dd67c4cb
yg <eng.yg@yahoo.com>
2023-10-26 18:00:23 +0800

    3.21 Deploying the database backend to production

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.port
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
