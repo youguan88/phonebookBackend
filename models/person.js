c7be5b5f5cc8aaed8a0a22c8937cdaab280ae4d5
yg <eng.yg@yahoo.com>
2023-10-26 20:31:08 +0800

    3.22: Lint configuration

--- models/person.js
+++ models/person.js
@@ -1,43 +1,44 @@
-const mongoose = require('mongoose')
-const url = process.env.MONGODB_URI
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+const mongoose = require('mongoose');
 
-mongoose.set('strictQuery',false)
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+mongoose.set('strictQuery', false);
+
+console.log('connecting to', url);
 mongoose.connect(url)
-.then(result =>{
-    console.log('connected to MongoDB')
-})
-.catch((error) => {
- console.log('error connecting to MongoDB:', error.message)
-})
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const personSchema = new mongoose.Schema({
-    "id": Number,
-    "name": {
-        type: String,
-        minLength: 3,
-        required: true
+  id: Number,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (value) => /^(\d{2}|\d{3})(-)(\d+)$/.test(value),
+      message: 'Invalid phone number input',
     },
-    "number": {
-        type: String,
-        validate: {
-            validator: (value) => {
-                return /^(\d{2}|\d{3})(\-)(\d+)$/.test(value)
-            }, 
-            message: "Invalid phone number input"
-        },
-        required: true,
-        minLength : 9
-    }
-})
+    required: true,
+    minLength: 9,
+  },
+});
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model('Person', personSchema);
